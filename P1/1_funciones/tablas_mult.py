e755b0221505d494b2dfff07e278778743fc171d
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2025-05-31 13:45:42 -0600

    tablas_mult.py

--- P1/1_funciones/tablas_mult.py
+++ P1/1_funciones/tablas_mult.py
@@ -4,3 +4,25 @@ restricciones:
 1. no estructuras de control
 2. no funciones
 '''
+
+num=int(input("Inserte el numero a multiplicar del 1 al 10: "))
+mult=num*1
+print(num,"* 1 = ", mult)
+mult=num*2
+print(num, "* 2 = ",mult)
+mult=num*3
+print(num, "* 3 = ",mult)
+mult=num*4
+print(num, "* 4 = ",mult)
+mult=num*5
+print(num, "* 5 = ",mult)
+mult=num*6
+print(num, "* 6 = ",mult)
+mult=num*7
+print(num, "* 7 = ",mult)
+mult=num*8
+print(num, "* 8 = ",mult)
+mult=num*9
+print(num, "* 9 = ",mult)
+mult=num*10
+print(num, "* 10 = ",mult)

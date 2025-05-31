5a7b8f7223f51c56ae6dbf26f3c6bc158eaf887e
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2025-05-31 14:26:08 -0600

    listas.py

--- P1/3_list/listas.py
+++ P1/3_list/listas.py
@@ -1,7 +1,34 @@
 #1 crear lista de numeros e imprimir el contenida
+numeros=[1,2,3,4,5]
+print(numeros)
 
 #2 crear una lista de palabras y posteriormente buscar la coincidencia de una palabra
+palabras=("hola","adios","si","no","idk","hola")
+buscar=input("Dame la palabra a buscar: ")
+if buscar in palabras:
+    print("sí esta la palabra en la lista")
+else:
+    print("no está la palabra en la lista")
+
 
 #3 añadir elementos a la lista
+numeros.append(6)
+print(numeros)
 
 #4 crear una lista multidimensional que permita almacenar el nombre y telefono de una agenda
+agenda=[
+    ["mama","1234567890"],
+    ["papa","0987654321"],
+    ["hermano","1122334455"],
+    ["amigo","5566778899"]
+]
+print(agenda)
+print("nuevo contacto: ")
+nombre=input("nombre: ")
+telefono=input("telefono: ")
+agenda.append([nombre, telefono])
+print(agenda)
+
+for contacto in agenda:
+    print(f"Nombre: {contacto[0]}, Telefono: {contacto[1]}")
+
